e2f7c07fdd3b2de1e9ba15c67d7ed485ac2bbab7
Adriano Costa <adriano.dmcosta@gmail.com>
2024-12-06 14:06:33 -0300

    Configurar backed e usar cidr para identificar as subredes usadas

--- provider.tf
+++ provider.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = "snacktech-backend-tfstate"
+    key    = "rds/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
 provider "aws" {
   region = var.regionDefault
 }
--- vars.tf
+++ vars.tf
@@ -14,10 +14,10 @@ variable "regionDefault" {
   default = "us-east-1"
 }
 
-variable "subnet01" {
-  default = "subnet-0f39f08004a478b4b" # example: subnet-abcdef1
+variable "subnet01Cidr" {
+  default = "172.31.0.0/20"
 }
 
-variable "subnet02" {
-  default = "subnet-060cf976106dda412" # example: subnet-abcdef1
+variable "subnet02Cidr" {
+  default = "172.31.32.0/20"
 }
