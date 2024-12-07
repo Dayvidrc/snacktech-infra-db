68dd21c138fd65cdad90c3ac57bb6fd287039ef1
Adriano Costa <adriano.dmcosta@gmail.com>
2024-12-07 11:56:17 -0300

    Infomar instancia para o script que cria o banco

--- Migrations/PowerShell/CheckAndCreateDatabase.ps1
+++ Migrations/PowerShell/CheckAndCreateDatabase.ps1
@@ -1,17 +1,19 @@
 param(
+    [Parameter(Mandatory = $true)]
+    [string]$Instance,
+    [Parameter(Mandatory = $true)]
+    [int]$Port,
     [Parameter(Mandatory=$true)]
     [string]$DbName,
-
     [Parameter(Mandatory=$true)]
     [string]$UID,
-
     [Parameter(Mandatory=$true)]
     [string]$Password
 )
 
 # Connect to the SQL Server instance
 $SqlConnection = New-Object System.Data.SqlClient.SqlConnection
-$SqlConnection.ConnectionString = "Server=your_server_instance;Database=master;User ID=$UID;Password=$Password;"
+$SqlConnection.ConnectionString = "Server=$Instance,$Port;Database=master;User ID=$UID;Password=$Password;"
 $SqlConnection.Open()
 
 # Check if the database exists
