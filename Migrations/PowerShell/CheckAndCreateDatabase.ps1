a746308a5ec0ed0419458f80f2ca03649a04b0ea
Adriano Costa <adriano.dmcosta@gmail.com>
2024-12-07 11:48:08 -0300

    configurar pipeline completo de deploy

---
+++ Migrations/PowerShell/CheckAndCreateDatabase.ps1
@@ -0,0 +1,29 @@
+param(
+    [Parameter(Mandatory=$true)]
+    [string]$DbName,
+
+    [Parameter(Mandatory=$true)]
+    [string]$UID,
+
+    [Parameter(Mandatory=$true)]
+    [string]$Password
+)
+
+# Connect to the SQL Server instance
+$SqlConnection = New-Object System.Data.SqlClient.SqlConnection
+$SqlConnection.ConnectionString = "Server=your_server_instance;Database=master;User ID=$UID;Password=$Password;"
+$SqlConnection.Open()
+
+# Check if the database exists
+$cmd = New-Object System.Data.SqlClient.SqlCommand
+$cmd.Connection = $SqlConnection
+$cmd.CommandText = "SELECT db_id('$DbName')"
+$reader = $cmd.ExecuteReader()
+
+if (!$reader.Read()) {
+    # Create the database
+    $cmd.CommandText = "CREATE DATABASE $DbName"
+    $cmd.ExecuteNonQuery()
+}
+
+$SqlConnection.Close()
